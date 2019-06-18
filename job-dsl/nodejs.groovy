3aeaf0513328d8e4dea1ea5cfbb6f2b1a6bc31b2
newb1e <ovst91@gmail.com>
2019-06-18 14:44:10 +0300

    change username and email to my own

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
         git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigName('newb1e')
+            node / gitConfigEmail('ovst91@gmail.com')
         }
     }
     triggers {
